b20b00b8af2513d45ef2d7c824ec9bc8d3ca4d7b
Omar Shabaan <omar.plus.077@gmail.com>
2022-12-07 23:38:08 +0200

    initial commit

---
+++ api/src/main/java/com/example/reoil/ReoilApplication.java
@@ -0,0 +1,13 @@
+package com.example.reoil;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReoilApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReoilApplication.class, args);
+	}
+
+}
